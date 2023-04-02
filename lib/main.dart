a3cb51e97ddc68793d8d2d2fe77407bd5a8e104a
TwiXyl9 <kolkin.2002@mail.com>
2023-04-02 23:02:59 +0300

    Change Cars_view by adding the Bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:transport/blocs/cars_bloc.dart';
 import 'package:transport/locator.dart';
 import 'package:transport/models/prefs.dart';
 import 'package:transport/views/home/home_view.dart';
@@ -18,14 +20,21 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Transport',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<CarsBloc>(
+          create: (context) => CarsBloc()..add(InitialCarEvent()),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Transport',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        initialRoute: '/',
+        home: LayoutTemplate(),
+        debugShowCheckedModeBanner: false,
       ),
-      initialRoute: '/',
-      home: LayoutTemplate(),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
--- lib/views/cars/cars_view.dart
+++ lib/views/cars/cars_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:transport/blocs/cars_bloc.dart';
 import 'package:transport/models/car.dart';
 import 'package:transport/requests/requests_paths_names.dart';
 import 'package:transport/services/api_service.dart';
@@ -8,42 +10,74 @@ class CarsView extends StatelessWidget {
   ApiService api = ApiService();
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<Car>>(
-      future: api.carsIndexRequest(carsPath),
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          print(snapshot.error);
-          return const Center(
-            child: Text('An error has occurred!'),
-          );
-        } else if (snapshot.hasData) {
-          return Column(
-                children: [
-                  Text("Наш Автопарк"),
-                  Expanded(
-                      child: Container(
-                        constraints: BoxConstraints(minWidth: 300, maxWidth: 800),
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Colors.black),
-                            borderRadius: BorderRadius.circular(10)),
-                        child: GridView.count(
-                            padding: EdgeInsets.zero,
-                            crossAxisCount: 1,
-                            shrinkWrap: true,
-                            children: snapshot.data!.map((e) => CarsItemView(e, (){})).toList()
-                        ),
-                      ),
-                  ),
-                ],
+    return BlocBuilder<CarsBloc, CarsState>(builder: (context, state) {
+      if (state is CarsInitial){
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      }
+      if (state is CarsLoaded){
+        return Column(
+          children: [
+            Text("Наш Автопарк"),
+            Expanded(
+              child: Container(
+                constraints: BoxConstraints(minWidth: 300, maxWidth: 800),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black),
+                    borderRadius: BorderRadius.circular(10)),
+                child: GridView.count(
+                    padding: EdgeInsets.zero,
+                    crossAxisCount: 1,
+                    shrinkWrap: true,
+                    children: state.cars.map((e) => CarsItemView(e, (){})).toList()
+                ),
+              ),
+            ),
+          ],
 
-          );
-
-        } else {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-      },
-    );
+        );
+      }
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    });
+    // return FutureBuilder<List<Car>>(
+    //   future: api.carsIndexRequest(carsPath),
+    //   builder: (context, snapshot) {
+    //     if (snapshot.hasError) {
+    //       print(snapshot.error);
+    //       return const Center(
+    //         child: Text('An error has occurred!'),
+    //       );
+    //     } else if (snapshot.hasData) {
+    //       return Column(
+    //             children: [
+    //               Text("Наш Автопарк"),
+    //               Expanded(
+    //                   child: Container(
+    //                     constraints: BoxConstraints(minWidth: 300, maxWidth: 800),
+    //                     decoration: BoxDecoration(
+    //                         border: Border.all(color: Colors.black),
+    //                         borderRadius: BorderRadius.circular(10)),
+    //                     child: GridView.count(
+    //                         padding: EdgeInsets.zero,
+    //                         crossAxisCount: 1,
+    //                         shrinkWrap: true,
+    //                         children: snapshot.data!.map((e) => CarsItemView(e, (){})).toList()
+    //                     ),
+    //                   ),
+    //               ),
+    //             ],
+    //
+    //       );
+    //
+    //     } else {
+    //       return const Center(
+    //         child: CircularProgressIndicator(),
+    //       );
+    //     }
+    //   },
+    // );
   }
 }
