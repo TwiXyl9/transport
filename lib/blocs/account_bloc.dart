f3d5d36837776781db9b51e5b08d715670c43746
TwiXyl9 <kolkin.2002@mail.com>
2023-05-05 13:58:34 +0300

    Create Update user info event in account bloc

--- lib/blocs/account_bloc.dart
+++ lib/blocs/account_bloc.dart
@@ -5,7 +5,10 @@ import 'dart:io';
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:path/path.dart';
+import 'package:transport/helpers/navigation_helper.dart';
+import 'package:transport/locator.dart';
 import 'package:transport/models/order.dart';
+import 'package:transport/routing/route_names.dart';
 import 'package:transport/services/api_service.dart';
 
 import '../data_provider/session_data_provider.dart';
@@ -17,7 +20,12 @@ import 'authentication_bloc.dart';
 
 @immutable
 abstract class AccountEvent {}
-class AccountInitialEvent extends AccountEvent {}
+class InitialAccountEvent extends AccountEvent {}
+class UpdateAccountEvent extends AccountEvent {
+  late User user;
+  UpdateAccountEvent(this.user);
+}
+class RedirectToMainPageAccountEvent extends AccountEvent {}
 
 @immutable
 abstract class AccountState {}
@@ -27,7 +35,10 @@ class AccountLoadedState extends AccountState {
   final User user;
   AccountLoadedState(this.user, this.orders);
 }
-class AccountWithoutUserState extends AccountState {}
+class AccountUpdateInProcessState extends AccountState {}
+class AccountUpdatedState extends AccountState {
+
+}
 class AccountFailureState extends AccountState {
   String error;
   AccountFailureState(this.error);
@@ -37,21 +48,27 @@ class AccountBloc extends Bloc<AccountEvent, AccountState> {
   final _sessionDataProvider = SessionDataProvider();
   final AuthenticationBloc authBloc;
   AccountBloc(this.authBloc) : super(AccountInitialState()) {
-    on<AccountInitialEvent>((event, emit) async {
-      await onInitialAccountEvent(event, emit);
+    on<AccountEvent>((event, emit) async {
+      if (event is InitialAccountEvent) {
+        await onInitialAccountEvent(event, emit);
+      } else if (event is UpdateAccountEvent) {
+        await onUpdateAccountEvent(event, emit);
+      } else if (event is RedirectToMainPageAccountEvent) {
+        await onRedirectToMainPageAccountEvent(event, emit);
+      }
     });
   }
-  onInitialAccountEvent(AccountInitialEvent event, Emitter<AccountState> emit) async {
+  onInitialAccountEvent(InitialAccountEvent event, Emitter<AccountState> emit) async {
     List<Order> orders = [];
     User user = new User.createGuest();
     var userId = await _sessionDataProvider.getAccountId();
     if (userId != null) {
-      orders = await ApiService().usersOrdersIndexRequest('/users/$userId/orders');
+      orders = await ApiService().usersOrdersIndexRequest('$usersPath/$userId/$ordersPath');
 
       var authString = await _sessionDataProvider.getAuthData();
       var authData = Auth.fromJson(jsonDecode(authString!));
       var authHeadersMap = authData.mapFromFields();
-      var result = await ApiService().userShowRequest('/users/${userId}', authHeadersMap);
+      var result = await ApiService().userShowRequest('$usersPath/${userId}', authHeadersMap);
       if(result.runtimeType == HttpException){
         authBloc.add(AuthenticationLogoutEvent());
         emit(AccountFailureState(result.toString()));
@@ -62,5 +79,28 @@ class AccountBloc extends Bloc<AccountEvent, AccountState> {
         emit(AccountLoadedState(user, orders));
       }
     }
+  }
+  onUpdateAccountEvent(UpdateAccountEvent event, Emitter<AccountState> emit) async {
+    try {
+      emit(AccountUpdateInProcessState());
+      var authString = await _sessionDataProvider.getAuthData();
+      var authData = Auth.fromJson(jsonDecode(authString!));
+      var authHeadersMap = authData.mapFromFields();
+      var result = await ApiService().updateUserRequest(usersPath, event.user, authHeadersMap);
+      print(result.runtimeType);
+      if (result.runtimeType != HttpException) {
+        _sessionDataProvider.deleteAuthData();
+        _sessionDataProvider.setAuthData(jsonEncode(authHeadersMap));
+        add(RedirectToMainPageAccountEvent());
+        add(InitialAccountEvent());
+      } else {
+        emit(AccountFailureState(result.toString()));
+      }
+    } catch (e) {
+      emit(AccountFailureState(e.toString()));
+    }
+  }
+  onRedirectToMainPageAccountEvent(RedirectToMainPageAccountEvent event, Emitter<AccountState> emit) async {
+    locator<NavigationHelper>().navigateTo(accountRoute);
   }
 }
