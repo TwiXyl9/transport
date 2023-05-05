39f0a651513ecda9a151e02bf09f4668b996bb1e
TwiXyl9 <kolkin.2002@mail.com>
2023-05-05 15:30:40 +0300

    Fix CustomCircularIndicator

--- lib/widgets/components/custom_circular_progress_indicator.dart
+++ lib/widgets/components/custom_circular_progress_indicator.dart
@@ -6,7 +6,7 @@ class CustomCircularProgressIndicator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: CustomCircularProgressIndicator(),
+      child: CircularProgressIndicator(),
     );
   }
 }
