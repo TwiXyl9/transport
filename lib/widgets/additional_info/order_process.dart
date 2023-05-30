5fc76044c16253e41b16ef87834ea8dd6f2d53ff
TwiXyl9 <kolkin.2002@mail.com>
2023-05-30 16:16:01 +0300

    Create horizontal listview for orderSteps

--- lib/widgets/additional_info/order_process.dart
+++ lib/widgets/additional_info/order_process.dart
@@ -1,37 +1,49 @@
 import 'package:flutter/material.dart';
 
+import '../components/page_header_text.dart';
+
 class OrderProcessWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 20),
-      child: GridView.count(
-        shrinkWrap: true,
-          crossAxisCount: MediaQuery.of(context).size.width > 600 ? 2 : 1 ,
-          children: [
-            _buildStepBlock('1', 'Создание заявки', 'Пользователь создает новую заявку, указывая информацию о грузе, маршруте и требованиях.'),
-            _buildArrow(),
-            _buildStepBlock('2', 'Подтверждение заявки', 'Компания проверяет заявку и подтверждает ее, общаясь с пользователем для уточнения деталей.'),
-            _buildArrow(),
-            _buildStepBlock('3', 'Назначение транспорта', 'Компания выбирает подходящий транспорт и назначает его для выполнения грузоперевозки.'),
-            _buildArrow(),
-            _buildStepBlock('4', 'Выполнение грузоперевозки', 'Загрузка груза, транспортировка в указанное место и разгрузка на месте назначения.'),
-            _buildArrow(),
-            _buildStepBlock('5', 'Подтверждение выполнения', 'Пользователь подтверждает выполнение заказа, оценивает качество услуг и оставляет отзыв.'),
-          ],
-      ),
+    return Column(
+      children: [
+        PageHeaderText(text: 'Шаги выполнения заказа'),
+        Container(
+            width: MediaQuery.of(context).size.width,
+            height: 170,
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: ListView(
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              children: [
+                _buildStepBlock('1', 'Создание заявки', 'Пользователь создает новую заявку, указывая информацию о грузе, маршруте и требованиях.'),
+                _buildArrow(),
+                _buildStepBlock('2', 'Подтверждение заявки', 'Компания проверяет заявку и подтверждает ее, общаясь с пользователем для уточнения деталей.'),
+                _buildArrow(),
+                _buildStepBlock('3', 'Назначение транспорта', 'Компания выбирает подходящий транспорт и назначает его для выполнения грузоперевозки.'),
+                _buildArrow(),
+                _buildStepBlock('4', 'Выполнение грузоперевозки', 'Загрузка груза, транспортировка в указанное место и разгрузка на месте назначения.'),
+                _buildArrow(),
+                _buildStepBlock('5', 'Подтверждение выполнения', 'Пользователь подтверждает выполнение заказа, оценивает качество услуг и оставляет отзыв.'),
+              ],
+            )
+        ),
+      ],
     );
   }
 
   Widget _buildStepBlock(String stepNumber, String title, String description) {
     return Container(
-      width: 10,
-      padding: EdgeInsets.symmetric(vertical: 20),
+      width: 250,
+      height: 170,
+      padding: EdgeInsets.all(10),
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(10)
       ),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Text(
             'Шаг $stepNumber',
