4f4e0201ab0bb3e2976510a3ef836e399941328c
TwiXyl9 <kolkin.2002@mail.com>
2023-06-02 14:55:20 +0300

    Change MapView. Add direction service for requests to Direction API and direction response for response parsing

--- lib/config/secrets.dart
+++ lib/config/secrets.dart
@@ -1,4 +1,5 @@
 class Secrets {
   static const API_KEY = 'AIzaSyBpRL6XpeALpLeUqdfWrsPTmLKnMmc52-k';
+  static const API_KEY_PLACES = 'AIzaSyAu6yEs4usuPbdoDqe5GzwjtwxoUszU4qk';
   static const CLIENT_ID = "1065041387667-9s3piggc339ttudh5hrnsscclk1p66qh.apps.googleusercontent.com";
 }
--- lib/widgets/map/map_view.dart
+++ lib/widgets/map/map_view.dart
@@ -1,12 +1,20 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_google_places/flutter_google_places.dart';
+import 'package:google_polyline_algorithm/google_polyline_algorithm.dart';
+import 'package:http/http.dart' as http;
 
 import 'package:google_map_polyline_new/google_map_polyline_new.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:google_maps_webservice/places.dart';
+import 'package:transport/services/direction_service.dart';
 
 import '../../config/secrets.dart';
+import '../../models/direction_response.dart';
+
+enum MarkerType {departure, arrival}
 
 class MapView extends StatefulWidget {
   const MapView({Key? key}) : super(key: key);
@@ -16,26 +24,29 @@ class MapView extends StatefulWidget {
 }
 
 class _MapViewState extends State<MapView> {
+  DirectionsResponse directionsResponse = DirectionsResponse();
   late GoogleMapController mapController;
-  TextEditingController departureController = new TextEditingController();
-  TextEditingController arrivalController = new TextEditingController();
+  TextEditingController departureController = TextEditingController();
+  TextEditingController arrivalController = TextEditingController();
   List<Marker> markersList = [];
-  final String key = Secrets.API_KEY;
-  LatLng _center = LatLng(53.677839, 23.829529);
-  GoogleMapsPlaces _places = GoogleMapsPlaces(
-    apiKey: Secrets.API_KEY,
-    baseUrl: 'https://maps.googleapis.com/maps/api',
-    apiHeaders: {
-      'X-Requested-With': 'XMLHttpRequest',
-      'Access-Control-Allow-Origin' : '*',
-    }
+  late String totalDistance;
+  late String totalTime;
+  bool routeCreated = false;
+  final String key = Secrets.API_KEY_PLACES;
+  final LatLng _center = const LatLng(53.677839, 23.829529);
+  final GoogleMapsPlaces _places = GoogleMapsPlaces(
+      apiKey: Secrets.API_KEY_PLACES,
+      baseUrl: 'https://maps.googleapis.com/maps/api',
+      apiHeaders: {
+        'X-Requested-With': 'XMLHttpRequest',
+        'Access-Control-Allow-Origin' : '*',
+      }
   );
-  GoogleMapPolyline googleMapPolyline = new GoogleMapPolyline(apiKey: Secrets.API_KEY);
-  final List<Polyline> polyline = [];
-  List<LatLng> routeCoords = [];
 
-  late PlaceDetails departure;
-  late PlaceDetails arrival;
+  final List<Polyline> polyline = [];
+
+  late PlaceDetails departure = PlaceDetails(name: '', placeId: '');
+  late PlaceDetails arrival = PlaceDetails(name: '', placeId: '');
 
   void onMapCreated(controller) {
     setState(() {
@@ -43,203 +54,178 @@ class _MapViewState extends State<MapView> {
     });
   }
 
-  Future<Null> displayPredictionDeparture(Prediction p) async {
-    if (p != null) {
-      // get detail (lat/lng)
-      PlacesDetailsResponse detail = await _places.getDetailsByPlaceId(
-          p.placeId!);
-      final lat = detail.result.geometry?.location.lat;
-      final lng = detail.result.geometry?.location.lng;
+  Future<void> displayPrediction(Prediction p, MarkerType type) async {
+    PlacesDetailsResponse detail = await _places.getDetailsByPlaceId(p.placeId!);
+    final lat = detail.result.geometry?.location.lat;
+    final lng = detail.result.geometry?.location.lng;
 
-      setState(() {
+    setState(() {
+      if (type == MarkerType.departure) {
         departure = detail.result;
-        departureController.text = detail.result.name;
-        Marker marker = Marker(
-            markerId: MarkerId('arrivalMarker'),
-            draggable: false,
-            infoWindow: InfoWindow(
-              title: "This is where you will arrive",
-            ),
-            onTap: () {
-              //print('this is where you will arrive');
-            },
-            position: LatLng(lat!, lng!)
-        );
-        markersList.add(marker);
-      });
-
-      mapController.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
-          target: LatLng(lat!, lng!),
-          zoom: 10.0
-      )));
-    }
-  }
-
-  Future<Null> displayPredictionArrival(Prediction p) async {
-    if (p != null) {
-      // get detail (lat/lng)
-      PlacesDetailsResponse detail = await _places.getDetailsByPlaceId(
-          p.placeId!);
-      final lat = detail.result.geometry?.location.lat;
-      final lng = detail.result.geometry?.location.lng;
-
-      setState(() {
+        departureController.text = detail.result.formattedAddress!;
+      } else {
         arrival = detail.result;
-        arrivalController.text = detail.result.name;
-        Marker marker = Marker(
-            markerId: MarkerId('arrivalMarker'),
-            draggable: false,
-            infoWindow: InfoWindow(
-              title: "This is where you will arrive",
-            ),
-            onTap: () {
-              //print('this is where you will arrive');
-            },
-            position: LatLng(lat!, lng!)
-        );
-        markersList.add(marker);
-      });
+        arrivalController.text = detail.result.formattedAddress!;
+      }
+      Marker marker = Marker(
+          markerId: type == MarkerType.departure? const MarkerId('departureMarker') : const MarkerId('arrivalMarker'),
+          draggable: false,
+          infoWindow: InfoWindow(
+            title: type == MarkerType.departure? "Точка отправления" : "Точка прибытия",
+          ),
+          onTap: () {
+            //print('this is where you will arrive');
+          },
+          position: LatLng(lat!, lng!)
+      );
+      markersList.add(marker);
+    });
 
-      mapController.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
-          target: LatLng(lat!, lng!),
-          zoom: 10.0
-      )));
+    moveCamera(lat!, lng!);
 
-      computePath();
-    }
+    computePath();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       width: 600,
       height: 400,
       child: Stack(
-          children: <Widget>[
-            GoogleMap(
-              onMapCreated: onMapCreated,
-              initialCameraPosition: CameraPosition(
-                target: _center,
-                zoom: 11.0,
-              ),
-              markers: Set.from(markersList),
-              polylines: Set.from(polyline),
+        children: <Widget>[
+          GoogleMap(
+            onMapCreated: onMapCreated,
+            initialCameraPosition: CameraPosition(
+              target: _center,
+              zoom: 11.0,
             ),
-            Positioned(
-                top: 10.0,
-                right: 15.0,
-                left: 15.0,
-                child: Column(
-                  children: <Widget>[
-                    Container(
-                        height: 50.0,
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10.0),
-                            color: Colors.white
+            markers: Set.from(markersList),
+            polylines: Set.from(polyline),
+          ),
+          Positioned(
+              top: 10.0,
+              right: 15.0,
+              left: 15.0,
+              child: Column(
+                children: <Widget>[
+                  AddressTextField('Точка погрузки', departureController, MarkerType.departure),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  AddressTextField('Точка выгрузки', arrivalController, MarkerType.arrival),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  if (routeCreated) Container(
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      children: [
+                        Text(
+                          "Расстояние: ${totalDistance}",
+                          style: const TextStyle(fontSize: 18),
                         ),
-                        child: Column(
-                          children: <Widget>[
-                            TextField(
-                              decoration: InputDecoration(
-                                hintText: 'Точка погрузки',
-                                border: InputBorder.none,
-                                contentPadding: EdgeInsets.only(
-                                    left: 15.0, top: 15.0),
-                                // suffixIcon: IconButton(
-                                //   icon: Icon(Icons.search),
-                                //   iconSize: 30.0,
-                                // )
-                              ),
-                              controller: departureController,
-                              onTap: () async {
-                                Prediction p = (await PlacesAutocomplete.show(
-                                    context: context,
-                                    apiKey: key,
-                                    offset: 0,
-                                    strictbounds: false,
-                                    types: [],
-                                    radius: 1000,
-                                    mode: Mode.overlay,
-                                    language: "ru",
-                                    components: [
-                                      new Component(Component.country, "ru")
-                                    ]))!;
-                                displayPredictionDeparture(p);
-                              },
-                              //onEditingComplete: searchAndNavigate,
-                            ),
-                          ],
-                        )
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Container(
-                        height: 50.0,
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10.0),
-                            color: Colors.white
+                        Text(
+                          "Время: ${totalTime}",
+                          style: const TextStyle(fontSize: 18),
                         ),
-                        child: Column(
-                          children: <Widget>[
-                            TextField(
-                              decoration: InputDecoration(
-                                hintText: 'Точка выгрузки',
-                                border: InputBorder.none,
-                                contentPadding: EdgeInsets.only(
-                                    left: 15.0, top: 15.0),
-                                // suffixIcon: IconButton(
-                                //   icon: Icon(Icons.search),
-                                //   iconSize: 30.0,
-                                // )
-                              ),
-                              controller: arrivalController,
-                              onTap: () async {
-                                Prediction? p = await PlacesAutocomplete.show(
-                                    context: context,
-                                    apiKey: key,
-                                    offset: 0,
-                                    strictbounds: false,
-                                    types: [],
-                                    radius: 1000,
-                                    mode: Mode.overlay,
-                                    language: "ru",
-                                    components: [
-                                      new Component(Component.country, "ru")
-                                    ]
-                                );
-                                displayPredictionArrival(p!);
-                              },
-                              //onEditingComplete: searchAndNavigate,
-                            ),
-                          ],
-                        )
+                      ],
                     ),
-                  ],
-                )
-            ),
-          ],
-        ),
+                  )
+                ],
+              )
+          ),
+        ],
+      ),
     );
   }
 
-  computePath() async{
-    LatLng origin = new LatLng(departure.geometry!.location.lat, departure.geometry!.location.lng);
-    LatLng end = new LatLng(arrival.geometry!.location.lat, arrival.geometry!.location.lng);
-    routeCoords.addAll((await googleMapPolyline.getCoordinatesWithLocation(origin: origin, destination: end, mode: RouteMode.driving))!);
+  Widget AddressTextField(String label, TextEditingController controller, MarkerType type){
+    return Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            color: Colors.white
+        ),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(
+                labelText: label,
+                border: InputBorder.none,
+                enabledBorder:  const OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white)
+                ),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey.shade400)
+                ),
+                fillColor: Colors.grey.shade200,
+                filled: true,
+                alignLabelWithHint: true,
+                labelStyle: const TextStyle(color: Colors.black),
+              ),
+              controller: controller,
+              onTap: () async {
+                Prediction p = (await PlacesAutocomplete.show(
+                    context: context,
+                    apiKey: key,
+                    offset: 0,
+                    strictbounds: false,
+                    types: [],
+                    radius: 1000,
+                    mode: Mode.overlay,
+                    language: "ru",
+                    components: [
+                      Component(Component.country, "by")
+                    ]))!;
+                if (p != null) displayPrediction(p, type);
+                FocusScope.of(context).requestFocus(FocusNode());
+              },
+              //onEditingComplete: searchAndNavigate,
+            ),
+          ],
+        )
+    );
+  }
+
+  void computePath() async {
+    if (departure.name.isEmpty || arrival.name.isEmpty) return;
+    LatLng origin = LatLng(departure.geometry!.location.lat, departure.geometry!.location.lng);
+    LatLng end = LatLng(arrival.geometry!.location.lat, arrival.geometry!.location.lng);
+
+
+
+    directionsResponse = await DirectionService().getSometing(Secrets.API_KEY_PLACES, origin, end);
+
+    totalDistance = directionsResponse.routes![0].legs![0].distance!.text!;
+    totalTime = directionsResponse.routes![0].legs![0].duration!.text!;
 
     setState(() {
-      polyline.add(Polyline(
-          polylineId: PolylineId('iter'),
-          visible: true,
-          points: routeCoords,
-          width: 4,
-          color: Colors.blue,
-          startCap: Cap.roundCap,
-          endCap: Cap.buttCap
-      ));
+      polyline.add(
+          Polyline(
+              polylineId: const PolylineId(
+                  'route'
+              ),
+              points: directionsResponse.routes![0].overviewPolyline!.points,
+              width: 5,
+              color: Colors.blue
+          )
+      );
+      routeCreated = true;
     });
+
+  }
+
+  void moveCamera(double lat, double lng) {
+    mapController.animateCamera(
+        CameraUpdate.newCameraPosition(
+            CameraPosition(
+                target: LatLng(lat, lng),
+                zoom: 10.0
+            )
+        )
+    );
   }
 }
